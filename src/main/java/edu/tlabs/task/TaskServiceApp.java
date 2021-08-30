c27ff905b3326560cdc84a06012a904f4f9456c1
Raghav Tenneti <Raghav Tenneti@DESKTOP-VF1S1LE>
2021-08-30 17:58:02 +0530

    Initial Commit Google Task Service

---
+++ src/main/java/edu/tlabs/task/TaskServiceApp.java
@@ -0,0 +1,13 @@
+package edu.tlabs.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskServiceApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskServiceApp.class, args);
+	}
+
+}
